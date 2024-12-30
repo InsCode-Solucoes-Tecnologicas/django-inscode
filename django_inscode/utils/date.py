c01d042a371a6eb552d6cc60a54c3e4d21b82fcf
Mateus Cavalcanti <mateuscavalcanti1607@gmail.com>
2024-12-30 17:31:42 -0300

    fix: parse_str_to_datetime now accepts str of date

--- django_inscode/utils/date.py
+++ django_inscode/utils/date.py
@@ -1,6 +1,6 @@
 from django.conf import settings as django_settings
 
-from datetime import datetime
+from datetime import datetime, date
 
 import settings
 import pytz
@@ -19,12 +19,21 @@ def get_actual_datetime() -> datetime:
 
 def parse_str_to_datetime(datetime_str: str) -> datetime:
     """
-    Converte uma string em um objeto datetime com base no formato especificado.
+    Converte uma string ou objeto date em um objeto datetime com base no formato especificado.
 
-    :param datetime_str: A string representando a data e hora.
+    :param datetime_str: A string representando a data e hora ou um objeto date.
     :return: Um objeto datetime convertido.
     :raises ValueError: Se a string não estiver no formato esperado.
+    :raises TypeError: Se o argumento não for uma string ou um objeto date.
     """
+    if isinstance(datetime_str, date) and not isinstance(datetime_str, datetime):
+        datetime_str = datetime_str.strftime("%Y-%m-%d 00:00:00")
+
+    elif not isinstance(datetime_str, str):
+        raise TypeError(
+            f"O argumento deve ser uma string ou um objeto date, mas foi recebido: {type(datetime_str).__name__}"
+        )
+
     try:
         _date = datetime.strptime(datetime_str, settings.DEFAULT_DATETIME_FORMAT)
         return _date
