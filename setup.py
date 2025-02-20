0790e7263438f3a0c0630a41b62372a25e1ba58c
Mateus Cavalcanti <mateuscavalcanti1607@gmail.com>
2025-02-20 17:20:53 -0300

    Update version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read(f):
 
 setup(
     name="django-inscode",
-    version="0.1.23",
+    version="0.1.24",
     description="Django framework da Inscode.",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
